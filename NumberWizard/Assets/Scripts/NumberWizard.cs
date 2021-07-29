7f0a0daf3ab2f1332b2c208b576a56ccc8a7d20c
Tonci Marinac <toncimarinac@gmail.com>
2021-07-29 18:37:08 +0200

    Start Menu UI scene, ep. 37

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
     // Start is called before the first frame update
     void Start() {
+        StartGame();
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to Number Wizard game!");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
@@ -24,16 +32,20 @@ public class NumberWizard : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return)) {
             Debug.Log("I am a genius!");
+            StartGame();
         }
+    }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than " + guess);
     }
 }
